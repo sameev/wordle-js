e063ccfd2c63ba81a2088d5b96da56b56a8e1eee
Samee Vohra <samee.vohra@gmail.com>
2023-04-03 18:02:06 -0600

    refactored add letter, added backspace functionality, and added fetch request for word of the day

--- app.js
+++ app.js
@@ -4,38 +4,55 @@ const ANSWER_LENGTH = 5;
 
 const init = async () => {
   let currentGuess = '';
+  let currentRow = 0;
+
+  const wordOfTheDay = await getWordOfTheDay();
+  console.log(wordOfTheDay);
 
   const isLetter = (letter) => {
     return /^[a-zA-Z]$/.test(letter);
   };
 
-  const commit = () => {
-    console.log('invoking commit function');
+  const commit = async () => {
+    if (currentGuess.length !== ANSWER_LENGTH) {
+      //do nothing
+      return;
+    }
+
+    //TODO: Need to validate that the word is a correct 5-letter word
+
+    //TODO: Mark the letter boxes of current row as "correct" "incorrect" or "wrong-spot"
+
+    //TODO: Check to see if the user won(correct guess) or lost (incorrect guess on 6th chance)
+
+    currentRow++;
+    currentGuess = '';
   };
 
   const backspace = () => {
-    console.log('invoking backspace function');
+    if (currentGuess.length > 0) {
+      currentGuess = currentGuess.slice(0, -1);
+      letters[currentRow * ANSWER_LENGTH + currentGuess.length].innerText = '';
+    }
   };
 
   const addLetter = (letter) => {
-    console.log('invoking addLetter function');
     if (currentGuess.length < ANSWER_LENGTH) {
       currentGuess += letter;
     } else {
-      //replace last letter of current guess if already 5 letters
+      //*replace last letter of current guess if already 5 letters
       currentGuess = currentGuess.slice(0, -1) + letter;
     }
 
-    //renders letter on browser
-    letters[currentGuess.length - 1].innerText = letter;
+    //*renders letter on browser
+    letters[currentRow * ANSWER_LENGTH + currentGuess.length - 1].innerText =
+      letter;
   };
 
   document.addEventListener(
     'keydown',
     (handleKeyPress = (event) => {
       const pressedKey = event.key;
-
-      console.log(pressedKey);
 
       if (pressedKey === 'Enter') {
         commit();
@@ -48,6 +65,13 @@ const init = async () => {
       }
     })
   );
+};
+
+const getWordOfTheDay = async () => {
+  const res = await fetch('https://words.dev-apis.com/word-of-the-day');
+  const data = await res.json();
+
+  return data.word.toUpperCase();
 };
 
 init();
